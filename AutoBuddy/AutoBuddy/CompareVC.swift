861a21476d62b76477370c431859e65a9dbf74c9
Jordan <jdkauffman11@gmail.com>
2017-03-18 22:47:35 -0500

    Finishing up CompareVC

--- AutoBuddy/AutoBuddy/CompareVC.swift
+++ AutoBuddy/AutoBuddy/CompareVC.swift
@@ -9,152 +9,128 @@
 import UIKit
 
 class CompareVC: UIViewController, UITextFieldDelegate, UITableViewDelegate, UITableViewDataSource {
-
+    
     @IBOutlet var year: UITextField!
     @IBOutlet var make: UITextField!
     @IBOutlet var model: UITextField!
     @IBOutlet var tableView: UITableView!
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
     
-    var trims = [String] ()
-
+    @IBOutlet var nextButton: UIButton!
+    
+    private var trims = [String]() // trims that the vehicle comes in
+    private var status = 1 // used to keep track of current status
+    private var trimDetails: NSArray = [] // holds the values of all trims. when one is selected, the relevant info is passed
+    private var selectedTrimIndex = 0 // index of the trim selected
+    private var vehicleOne: String!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        titleLabel.text = "Step \(status): Enter vehicle information"
+        subtitleLabel.text = "Vehicle \(status):"
+        nextButton.isHidden = true
         
         year.delegate = self
         make.delegate = self
         model.delegate = self
         
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        tableView.separatorColor = UIColor.white
+        tableView.backgroundColor = UIColor.clear
         tableView.isHidden = true
-
+        
+        
+        
         // Do any additional setup after loading the view.
     }
     
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.trims.count;
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
-        
-        cell.textLabel?.text = self.trims[indexPath.row]
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("You selected cell #\(indexPath.row)!")
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     
     @IBAction func loadTrims(_ sender: Any) {
         dismissKeyboard()
         if make.text != "" && model.text != "" && year.text != ""
         {
             getJSONData(path: "https://api.edmunds.com/api/vehicle/v2/" + make.text! + "/" + model.text! + "/" + year.text! + "?fmt=json&api_key=")
+            self.nextButton.isHidden = false
+        }
+    }
+    
+    @IBAction func nextScreen(_ sender: Any) {
+        status += 1
+        if status == 2
+        {
+            titleLabel.text = "Step \(status): Enter vehicle information"
+            subtitleLabel.text = "Vehicle \(status):"
+//CHECK THIS CONDITION
+            // Save data and pull any additional data, clear fields
+            if trimDetails.count != 0
+            {
+                // save data
+                let toBeSent = trimDetails[selectedTrimIndex]
+                vehicleOne = "\(self.year.text) \(self.make) \(self.model)"
+                
+                // clear the fields
+                self.year.text = ""
+                self.make.text = ""
+                self.model.text = ""
+                self.trimDetails = [] // would be reset anyway
+                
+                // Hide buttons
+                nextButton.isHidden = true
+                
+                
+            }
+        }
+        else
+        {
+            // Segue (Send two strings of vehicle data as well as year make and model)
+            // Add code to send data to VC
+            performSegue(withIdentifier: "compare", sender: nil)
         }
     }
     
     func getJSONData(path: String)
     {
-        // prepare json data
-        let json = "{\"id\":\"ABC\", \"year\": \"2000\", \"styles\":[{\"id\":\"ABC\", \"name\":\"ABC\", \"submodel\":{\"body\":\"ABC\", \"modelName\":\"ABC\",\"niceName\":\"ABC\"} \"trim\":\"ABC\"}]}"
-        //var data = json.data(using: .utf8)!
-        //let json: [String: Any] = ["id": "ABC", "dict": ["1":"First", "2":"Second"]]
-        
-        let jsonData = try? JSONSerialization.data(withJSONObject: json)
-
-        // create post request
+        let config = URLSessionConfiguration.default // Session Configuration
+        let session = URLSession(configuration: config) // Load configuration into Session
         let url = URL(string: path + "mbaawxhjajwzsqs7pgxnbefj")!
-        var request = URLRequest(url: url)
-        request.httpMethod = "GET"
         
-        // insert json data to the request
-        request.httpBody = jsonData
-        
-        let task = URLSession.shared.dataTask(with: request)
-        {
-            data, response, error in
-            guard let data = data, error == nil else
+        let task = session.dataTask(with: url, completionHandler:
             {
-                print(error?.localizedDescription ?? "No data")
-                return
-            }
-            let responseJSON = try? JSONSerialization.jsonObject(with: data, options: [])
-            if let responseJSON = responseJSON as? [String: Any]
-            {
-                print(responseJSON)
-                self.parseJSON(json: responseJSON)
-                self.tableView.isHidden = false
-            }
-        }
-        
+                (data, response, error) in
+                if error != nil
+                {
+                    print(error!.localizedDescription)
+                }
+                else
+                {
+                    do {
+                        let resultJSON = try JSONSerialization.jsonObject(with: data!) as! [String: Any]
+                        // Parse the data in the result
+                        let styles = resultJSON["styles"] as! NSArray
+                        self.trimDetails = styles
+                        for i in 0..<styles.count
+                        {
+                            let name = styles[i] as! Dictionary<String, Any>
+                            self.trims.append(name["name"] as! String)
+                        }
+                        DispatchQueue.main.async() { // Async task to handle UI updating
+                            self.tableView.reloadData()
+                            self.tableView.isHidden = false
+                            return
+                        }
+                    }
+                    catch
+                    {
+                        print("Error: \(error)")
+                    }
+                }
+        })
         task.resume()
         
-//        let config = URLSessionConfiguration.default // Session Configuration
-//        let session = URLSession(configuration: config) // Load configuration into Session
-//        let url = URL(string: path + "mbaawxhjajwzsqs7pgxnbefj")!
-//        
-//        let task = session.dataTask(with: url, completionHandler:
-//            {
-//            (data, response, error) in
-//            
-//            if error != nil
-//            {
-//                
-//                print(error!.localizedDescription)
-//                
-//            }
-//            else
-//            {
-//                do {
-//                    if let resultJSON = try JSONSerialization.jsonObject(with: data!, options: .allowFragments) as? [String: Any]
-//                    {
-//                        //Implement your logic
-//                        print(resultJSON)
-//                        
-//                        DispatchQueue.main.async(){
-//                            self.parseJSON(json: resultJSON)
-//                            self.tableView.isHidden = false
-//                        }
-//                    }
-//                }
-//                catch
-//                {
-//                    
-//                    print("error in JSONSerialization")
-//                    
-//                }
-//                
-//                
-//            }
-//            
-//        })
-//        task.resume()
-    }
-    
-    func parseJSON(json: [String: Any])
-    {
-//        if let styles = json["styles"] as? [[String: Any]]
-//        {
-//            for style in styles
-//            {
-//                if let names = style["name"]
-//                {
-//                    for name in names
-//                    {
-//                        trims.append(name)
-//                    }
-//                    
-//                }
-//            }
-//        }
-        tableView.reloadData()
     }
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -171,16 +147,41 @@ class CompareVC: UIViewController, UITextFieldDelegate, UITableViewDelegate, UIT
         view.endEditing(true)
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.trims.count;
     }
-    */
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = self.tableView.dequeueReusableCell(withIdentifier: "cell")! as UITableViewCell
+        
+        cell.textLabel?.text = self.trims[indexPath.row]
+        cell.textLabel?.textColor = UIColor.white
+        cell.contentView.backgroundColor = UIColor.clear
+        cell.backgroundColor = UIColor.clear
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        selectedTrimIndex = indexPath.row
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
     
     
-
+    
 }
